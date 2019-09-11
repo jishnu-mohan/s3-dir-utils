0e9c82d86ebcde6d114ec55bb728baaa4cd1bd48
jishnu-mohan <jishnumpr@gmial.com>
2019-09-11 14:03:58 +0530

    - added 'fileExists' function

--- index.js
+++ index.js
@@ -15,10 +15,13 @@ async function getStructure(options) {
     s3.listObjectsV2(params, async function (err, data) {
       if(err) return reject(err)
       if(data.hasOwnProperty('Contents') && data.Contents.length === 0) {
-        return reject(`bucket "${bucket}" is empty`)
+        if(options.hasOwnProperty('folder')) 
+          return reject (`${prefix} is empty or no such folder exists`)
+        else 
+          return reject(`${bucket} is empty or no such bucket exists`)
       }
       else {
-       let bucketContents = await getBucketContents(data.Contents)
+        let bucketContents = await getBucketContents(data.Contents)
         return resolve(bucketContents)
       }
     })
@@ -63,4 +66,49 @@ async function getFolders(key, parentObject) {
   }
 }
 
+async function fileExists(options, file) {
+  return new Promise((resolve, reject) => {
+    if (!options.hasOwnProperty('s3') || options.s3 === null) { return reject('Missing required parameter s3') }
+    if (!options.hasOwnProperty('bucket') || options.bucket === null) { return reject('Missing required parameter bucket') }
+    if (!file || file === null) { return reject('Missing required parameter file name') }
+    let prefix = (!options.hasOwnProperty('folder') || options.folder === null)? '' : String(options.folder)
+    let s3 =  options.s3
+    let bucket = options.bucket
+
+    let params = { 
+      Bucket: bucket,
+      Prefix: prefix
+    }
+    
+    s3.listObjectsV2(params, async function (err, data) {
+      if(err) return reject(err)
+      if(data.hasOwnProperty('Contents') && data.Contents.length === 0) {
+        if(options.hasOwnProperty('folder')) 
+          return reject (`${prefix} is empty or no such folder exists`)
+        else 
+          return reject(`${bucket} is empty or no such bucket exists`)
+      }
+      else {
+        let files = await getFiles(data.Contents)
+        return resolve (files.includes(file))
+      }
+    })
+  })
+}
+
+async function getFiles(contents) {
+  let files = []
+  contents.forEach((object) => {
+    let key = object.Key
+    if(key && key.includes('/')) {
+      files.push(key.split('/').pop())
+    }
+    else {
+      files.push(key)
+    }    
+   })
+   return files
+}
+
 exports.getStructure = getStructure
+exports.fileExists = fileExists
