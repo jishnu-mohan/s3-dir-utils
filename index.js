f3140d349d3e748a34de3c712719d9068faa5ac0
jishnu-mohan <jishnumpr@gmial.com>
2019-09-11 18:00:02 +0530

    - Added getAllFiles

--- index.js
+++ index.js
@@ -96,6 +96,36 @@ async function fileExists(options, file) {
   })
 }
 
+async function getAllFiles(options, file) {
+  return new Promise((resolve, reject) => {
+    if (!options.hasOwnProperty('s3') || options.s3 === null) { return reject('Missing required parameter s3') }
+    if (!options.hasOwnProperty('bucket') || options.bucket === null) { return reject('Missing required parameter bucket') }
+    if (!file || file === null) { return reject('Missing required parameter file name') }
+    let prefix = (!options.hasOwnProperty('folder') || options.folder === null)? '' : String(options.folder)
+    let s3 =  options.s3
+    let bucket = options.bucket
+
+    let params = { 
+      Bucket: bucket,
+      Prefix: prefix
+    }
+    
+    s3.listObjectsV2(params, async function (err, data) {
+      if(err) return reject(err)
+      if(data.hasOwnProperty('Contents') && data.Contents.length === 0) {
+        if(options.hasOwnProperty('folder')) 
+          return reject (`${prefix} is empty or no such folder exists`)
+        else 
+          return reject(`${bucket} is empty or no such bucket exists`)
+      }
+      else {
+        let files = await getFiles(data.Contents)
+        return resolve (files)
+      }
+    })
+  })
+}
+
 async function getFiles(contents) {
   let files = []
   contents.forEach((object) => {
@@ -112,3 +142,4 @@ async function getFiles(contents) {
 
 exports.getStructure = getStructure
 exports.fileExists = fileExists
+exports.getAllFiles = getAllFiles
